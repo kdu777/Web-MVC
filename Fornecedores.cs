aa4897299800601b19b0be80032b9e6fdda799f6
kdu777 <118766036+kdu777@users.noreply.github.com>
2023-03-01 18:53:56 -0300

    Create Fornecedores.cs

---
+++ Fornecedores.cs
@@ -0,0 +1,15 @@
+namespace LHPet.Models;
+classe pública Fornecedores{
+    public int ID { get; definir; }
+string pública Nome {obter; definir; }
+string pública Cnpj { obter; definir; }
+public string E-mail { get; definir; }
+public Fornecedor(int id, string nome, string cnpj,
+cadeia de e-mail)
+{
+this.Id = id;
+this.Nome = nome;
+this.Cnpj = cnpj;
+this.Email = email;
+}
+}
